38e4c547bc0bb5cb62423ff17f2deb6d6ce0f6f4
kento.uoi <fishperson19@gmail.com>
2021-02-03 00:46:47 +0900

    mongoDB実装

---
+++ models/note.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('mongoに変数を渡しました', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(result => {
+    console.log('mongoコネクト')
+  })
+  .catch((error) => {
+    console.log('mongoコネクトエラー', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean,
+})
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+module.exports = mongoose.model('Note', noteSchema)
