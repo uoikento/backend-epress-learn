7129f90db6e8d8ab38c913ed14ac74366a77f1a4
kento.uoi <fishperson19@gmail.com>
2021-02-07 00:28:55 +0900

    addEslint

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-
 app.use(errorHandler)
 
 app.get('/api/notes', (request, response) => {
@@ -39,32 +38,29 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-  .then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error)
-  )
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error)
+    )
 })
 
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
-  
   if (body.content === undefined) {
     return response.status(400).json({
       error: '内容ミス'
     })
   }
-  
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   })
-  
   note
     .save()
     .then(savedNote => savedNote.toJSON())
@@ -74,31 +70,29 @@ app.post('/api/notes', (request, response, next) => {
     .then(savedAndFormattedNote => {
       response.json(savedAndFormattedNote)
     })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
   const body = request.body
-  
   const note = {
     constent: body.content,
     important: body.important,
   }
-  
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-  .then(updatedNote => {
-    response.json(updatedNote)
-    console.log(updatedNote)
-  })
-  .catch(error => next(error))
+    .then(updatedNote => {
+      response.json(updatedNote)
+      console.log(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
   Note.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
